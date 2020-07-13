4191fffa95408043ab5c7f1044b8bff1de3af6f0
daonguyyen <mdnguyen928@gmail.com>
2020-07-13 14:24:57 +0700

    7 - Su dung Axios goi API

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList'
 import ProductItem from './../../components/ProductItem/ProductItem'
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import axios from 'axios';
+
 class ProductListPage extends Component {
     render() {
-        var {products} = this.props;
+        // var {products} = this.props;
+        var products = []
+
+        axios({
+            method : 'get',
+            url : 'http://localhost:3000/products'
+        }).then(res => {
+            console.log(res);
+            products = res.data;
+        }).catch(err => {
+            console.log(err);
+        });
+
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <button type="button" className="btn btn-info m-b-10">Thêm sản phẩm</button>
