81da5c5833eca44c4f634f092084f07586cba52e
daonguyyen <mdnguyen928@gmail.com>
2020-07-13 14:43:16 +0700

    7 - Connect API : Tách Axios Xử Lý Chung

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-import ProductList from './../../components/ProductList/ProductList'
-import ProductItem from './../../components/ProductItem/ProductItem'
-import {connect} from 'react-redux';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import ProductItem from './../../components/ProductItem/ProductItem';
+import ProductList from './../../components/ProductList/ProductList';
+import callAPI from './../../utils/apiCaller';
 
 class ProductListPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            products : []
+        }
+    }
+
+    componentDidMount() {
+        callAPI('products', 'GET', null).then (res => {
+            this.setState({
+                products : res.data
+            })
+        })
+    }
+    
+    
+
     render() {
         // var {products} = this.props;
-        var products = []
-
-        axios({
-            method : 'get',
-            url : 'http://localhost:3000/products'
-        }).then(res => {
-            console.log(res);
-            products = res.data;
-        }).catch(err => {
-            console.log(err);
-        });
+        var {products} = this.state;
 
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
